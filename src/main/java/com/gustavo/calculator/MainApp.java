df32feed3694adf9b058e3cb5481282adcf2466a
Gustavo De Jesus Silva <gustavo.jesus.silva.dev@gmail.com>
2026-06-02 17:04:10 -0300

    feat: criei os botões da calculadora simples e implementei a classe main refact: corrigi o pom.xml do arquivo

--- src/main/java/com/gustavo/calculator/MainApp.java
+++ src/main/java/com/gustavo/calculator/MainApp.java
@@ -1,4 +1,24 @@
 package com.gustavo.calculator;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/fxml/SimpleCalculator.fxml")
+        );
+
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
