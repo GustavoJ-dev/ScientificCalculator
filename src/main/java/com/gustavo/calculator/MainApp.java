baffd22e747534484d3241513d02b4788f14065b
Gustavo De Jesus Silva <gustavo.jesus.silva.dev@gmail.com>
2026-08-19 17:34:43 -0300

    feat: Segund parte da calculadora pronta

--- src/main/java/com/gustavo/calculator/MainApp.java
+++ src/main/java/com/gustavo/calculator/MainApp.java
@@ -10,7 +10,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                getClass().getResource("/fxml/SimpleCalculator.fxml")
+                getClass().getResource("/fxml/ScientificCalculator.fxml")
         );
 
         Scene scene = new Scene(fxmlLoader.load());
