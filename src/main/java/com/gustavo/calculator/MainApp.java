7e9c4fab0584476f6dc2bb7accc71fe97ae77e04
Gustavo De Jesus Silva <gustavo.jesus.silva.dev@gmail.com>
2026-09-08 17:52:08 -0300

    feat: estilização concluida, feat: primeira versão de lógica dos operando aplicada

--- src/main/java/com/gustavo/calculator/MainApp.java
+++ src/main/java/com/gustavo/calculator/MainApp.java
@@ -2,14 +2,17 @@ package com.gustavo.calculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/ScientificCalculator.fxml"));
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/SimpleCalculator.fxml"));
+
+        Font.loadFont(getClass().getResourceAsStream("/fonts/PressStart2P.ttf"), 16);
 
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("/css/calculator.css").toExternalForm());
--- src/main/java/com/gustavo/calculator/controller/CalculatorController.java
+++ src/main/java/com/gustavo/calculator/controller/CalculatorController.java
@@ -1,4 +1,73 @@
 package com.gustavo.calculator.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
 public class CalculatorController {
+
+    @FXML
+    private Label expressionLabel;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    public void initialize(){
+
+        resultLabel.setText("0");
+    }
+
+    @FXML
+    private void handleNumber(ActionEvent event){
+
+        Button button = (Button) event.getSource();
+
+        String value = button.getText();
+
+        String currentText = resultLabel.getText();
+
+        if (currentText.equals("0")){
+
+            resultLabel.setText(value);
+        } else {
+            resultLabel.setText(currentText + value);
+        }
+    }
+
+
+    @FXML
+    private void switchToScientific(ActionEvent event) throws Exception{
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getResource("/fxml/ScientificCalculator.fxml")));
+
+        Scene scene = ((Node) event.getSource()).getScene();
+        scene.setRoot(root);
+
+        Stage stage = (Stage) scene.getWindow();
+        stage.sizeToScene();
+
+    }
+
+    @FXML
+    private void switchToSimple(ActionEvent event) throws Exception{
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getResource("/fxml/SimpleCalculator.fxml")));
+
+        Scene scene = ((Node) event.getSource()).getScene();
+        scene.setRoot(root);
+
+        Stage stage = (Stage) scene.getWindow();
+        stage.sizeToScene();
+    }
 }
